78a491dde0c6587d9e6b66f61a9b30f915a8eafe
mossyuser <155192370+minimos11@users.noreply.github.com>
2024-10-20 23:13:38 +0200

    Update index.js
    Now added the info list for the programs list. And will also be used for a start menu if that ever come to be.

--- ProgramFiles/index.js
+++ ProgramFiles/index.js
@@ -1,16 +1,29 @@
-const programList = [
+const programFilesList = [
     'ExampleProgram/MyProgram.js',
     
     //'path/to/your/program2.js',
     // Add here the address to your programs js file (Relative).
 ];
 
+const programInfoList = [
+    ['myprogram', 'My program', '/resources/code-sub-pages/window.ico'],
+    
+    //['function name', 'common name', 'icon']
+    // Add here your programs function name, its call/common name (like taskmgr becomes Task Manager), and icon is optional.
+];
+
 // Function to load scripts
-function loadPrograms(programs) {
-    programs.forEach(src => {
+function loadPrograms() {
+    programFilesList.forEach(src => {
         const script = document.createElement('script');
         script.src = 'https://minimos11.github.io/DesktopPrograms/ProgramFiles/' + src; //Makes the adress absolute and adds it to src of the script tag. 
         document.head.appendChild(script);
     });
+
+    programInfoList.forEach(Info => {
+        document.getElementById("programList").insertAdjacentHTML("beforeend", `<li><button onclick="` + Info[0] + `(); DisplayWindow('` + Info[0] + `')"><img width="16px" height="16px" src="` + Info[2] + `">` + Info[0] + `.exe` + `</button></li>`)
+    });
 }
-loadPrograms(programList);
+document.addEventListener("DOMContentLoaded", function() {
+    loadPrograms();
+});
