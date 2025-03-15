fd9063b6226cfa0bd8fc6a17b034f3b97f464ff4
mossyuser <155192370+minimos11@users.noreply.github.com>
2025-03-15 19:37:15 +0100

    Update MyProgram.js
    For the next update

--- ProgramFiles/ExampleProgram/MyProgram.js
+++ ProgramFiles/ExampleProgram/MyProgram.js
@@ -1,5 +1,5 @@
-function myprogram(){
-    generateWindow("", "myprogram", "My program", "/resources/code-sub-pages/window.ico", "");
+function myprogram$dex(){
+    generateWindow(null, "myprogram", "My program", "resources/window.ico", "");
     document.querySelector('#myprogram .window-body').innerHTML = "<h3>My sweet program</h3>";
     let window = document.getElementById('myprogram');
     window.style.height = "170px";
